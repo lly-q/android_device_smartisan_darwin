960cc49e970d4f536330c34e77c0d5807d76b558
loserq <2650009628@qq.com>
2022-07-22 16:47:41 +0800

    Darwin: Inital project
    We Hope have a good result.
    
    Signed-off-by: loserq <2650009628@qq.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_darwin.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_darwin-user \
+    omni_darwin-userdebug \
+    omni_darwin-eng
