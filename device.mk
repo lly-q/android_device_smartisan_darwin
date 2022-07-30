835c955e20584116a545b712882acd1547f498ee
EndCredits <alicization.han@gmail.com>
2022-07-30 13:28:01 +0800

    darwin: Build fastboot HAL & fastbootd
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -1,9 +1,13 @@
-#
+
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
 
 LOCAL_PATH := device/deltainno/darwin
 # A/B
